4957f48a2abca74652b453c20e7e2a9b07bad3b3
Zokirov Farrukh <mr.frank220599@gmail.com>
2019-02-19 17:12:16 +0500

    fixed issue

---
+++ precache-manifest.4c1e7780c86628307e1e3e09cc7323d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "380ac233aa5a8174876b",
+    "url": "/static/css/main.ba1807ca.chunk.css"
+  },
+  {
+    "revision": "380ac233aa5a8174876b",
+    "url": "/static/js/main.380ac233.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f99c8cea3318eff09689",
+    "url": "/static/js/2.f99c8cea.chunk.js"
+  },
+  {
+    "revision": "2ac56f09395580e3b6a838f729027fb1",
+    "url": "/static/media/24924-1920x1200.2ac56f09.jpg"
+  },
+  {
+    "revision": "2c9370f9ce21b0eae83c3cfff210c970",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37b9368a1265c85d0e65aca85d213ddb.js"
+  "/precache-manifest.4c1e7780c86628307e1e3e09cc7323d7.js"
 );
 
 workbox.clientsClaim();
